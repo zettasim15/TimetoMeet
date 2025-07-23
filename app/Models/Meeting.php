5c5bf3bf3142f54672897ccc144f72514248c70c
gisella1109 <gisellalaudila42@gmail.com>
2025-07-23 09:00:27 +0700

    Menambahkan Gisella Laudila sebagai kontributor di model Meeting

--- app/Models/Meeting.php
+++ app/Models/Meeting.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Kontributor: Gisella Laudila - Dokumentasi model Meeting
+ */
 class Meeting extends Model
 {
     protected $table = 'meetings';
